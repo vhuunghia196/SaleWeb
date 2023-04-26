0dc3a578076869ea9de553564e597166f410382d
Haunguyen42193 <haunguyen42193@gmail.com>
2023-04-26 23:21:21 +0700

    update-26/04/2024 11:21

---
+++ NiVi_Shop/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NiVi_Shop.Areas.Admin.Controllers
+{
+    public class OrderController : Controller
+    {
+        // GET: Admin/Order
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- NiVi_Shop/Areas/Admin/Controllers/StatisticsController.cs
+++ NiVi_Shop/Areas/Admin/Controllers/StatisticsController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using NiVi_Shop.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,8 +10,28 @@ namespace NiVi_Shop.Areas.Admin.Controllers
 {
     public class StatisticsController : Controller
     {
+        DBContextNiViShop dbConnect = new DBContextNiViShop();
         // GET: Admin/Statistics
         public ActionResult Index()
+        {
+            return View();
+        }
+
+        public class ProductInfo
+        {
+            public string CategoryName { get; set; }
+            public int Quantity { get; set; }
+            public double Revenue { get; set; }
+        }
+
+        [HttpGet]
+        public ActionResult getStatistics()
+        {
+            var products = dbConnect.Database.SqlQuery<ProductInfo>("SELECT c.CategoryName AS CategoryName, SUM(od.Quantity) AS Quantity, SUM(od.Price) AS Revenue FROM OrderDetail od INNER JOIN Products p ON od.ProductID = p.ProductID INNER JOIN Categories c ON p.CategoryID = c.CategoryID GROUP BY c.CategoryName").ToList();
+            return Json(products, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult Revenue()
         {
             return View();
         }
